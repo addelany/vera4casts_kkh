bb0b89137dff00eefbe2f4b19b6d15ddb4dec14c
Katie Hoffman <github.com/62773703+kjkhoffman@users.noreply.github.com>
2024-05-01 14:02:14 -0400

    running now!

--- R/generate_secchi_forecast.R
+++ R/generate_secchi_forecast.R
@@ -3,7 +3,8 @@ generate_secchi_forecast <- function(forecast_date, # a recommended argument so 
                                       model_id,
                                       targets_url, # where are the targets you are forecasting?
                                       horizon = 30, #how many days into the future
-                                      variable_collect = c("Secchi_m_sample", "Chla_ugL_mean"),
+                                      variable_collect = c("Secchi_m_sample"),
+                                      variable = c("Secchi_m_sample"),
                                       site, # what site(s)
                                       project_id = 'vera4cast') {
 
@@ -29,44 +30,44 @@ generate_secchi_forecast <- function(forecast_date, # a recommended argument so 
   # and you can specify the length of the future period and number of days in the past
   # you can modify the data that are collected in the get_daily_weather function
   # or if you want to generate an hourly forecast, you can use get_hourly_weather
-  weather_dat <- site |>
-    map_dfr(get_daily_weather, site_list = site_list, past = 90, future = 30, vars = "temperature_2m")
+  # weather_dat <- site |>
+  #   map_dfr(get_daily_weather, site_list = site_list, past = 90, future = 30, vars = "temperature_2m")
   #-------------------------------------
 
   # split it into historic and future
-  historic_weather <- weather_dat |>
-    filter(datetime < forecast_date) |>
-    # calculate a daily mean (remove ensemble)
-    group_by(datetime, variable, site_id) |>
-    summarise(prediction = mean(prediction)) |>
-    pivot_wider(names_from = variable, values_from = prediction) |>
-    mutate(air_temperature = air_temperature - 273.15)
+  # historic_weather <- weather_dat |>
+  #   filter(datetime < forecast_date) |>
+  #   # calculate a daily mean (remove ensemble)
+  #   group_by(datetime, variable, site_id) |>
+  #   summarise(prediction = mean(prediction)) |>
+  #   pivot_wider(names_from = variable, values_from = prediction) |>
+  #   mutate(air_temperature = air_temperature - 273.15)
   #
-  forecast_weather <- weather_dat |>
-    filter(datetime >= forecast_date) |>
-    pivot_wider(names_from = variable, values_from = prediction) |>
-    mutate(air_temperature = air_temperature - 273.15)
+  # forecast_weather <- weather_dat |>
+  #   filter(datetime >= forecast_date) |>
+  #   pivot_wider(names_from = variable, values_from = prediction) |>
+  #   mutate(air_temperature = air_temperature - 273.15)
   #-------------------------------------
 
   # Fit model
   # message('Fitting model')
 
-targets_test <- targets %>%
-  mutate(date = date(datetime)) %>%
-  select(-c(datetime)) %>%
-  rename("datetime" = "date") %>%
-  select(-c("depth_m"))
-targets_test_2 <- aggregate(observation ~ datetime + site_id + duration + variable + project_id, data = targets_test, mean)
+# targets_test <- targets %>%
+#   mutate(date = date(datetime)) %>%
+#   select(-c(datetime)) %>%
+#   rename("datetime" = "date") %>%
+#   select(-c("depth_m"))
+# targets_test_2 <- aggregate(observation ~ datetime + site_id + duration + variable + project_id, data = targets_test, mean)
 
 
-  fit_df <- targets_test_2 |>
-    #filter(datetime >= "2024-01-01") %>%
-    distinct() %>%
-    pivot_wider(names_from = variable, values_from = observation) #|>
-    # left_join(historic_weather, by = join_by(site_id, datetime))
-  fit_df <- na.omit(fit_df)
+  # fit_df <- targets_test_2 |>
+  #   #filter(datetime >= "2024-01-01") %>%
+  #   distinct() %>%
+  #   pivot_wider(names_from = variable, values_from = observation) #|>
+  #   # left_join(historic_weather, by = join_by(site_id, datetime))
+  # fit_df <- na.omit(fit_df)
   #
-   model_fit <- lm(fit_df$Secchi_m_sample ~ fit_df$Chla_ugL_mean)
+   # model_fit <- lm(fit_df$Secchi_m_sample ~ fit_df$Chla_ugL_mean)
   # #-------------------------------------
 
   #make model
@@ -79,9 +80,9 @@ targets_test_2 <- aggregate(observation ~ datetime + site_id + duration + variab
     cbind(horizon_dates) %>%     #add values to dates
     pivot_longer(names_to = "parameter", values_to = "prediction", cols = c(mu, sigma))
 
-  forecasted_secchi_2 <- targets %>%
-    select(observation) %>%
-    na.omit() #%>%
+  # forecasted_secchi_2 <- targets %>%
+  #   select(observation) %>%
+  #   na.omit() #%>%
 
   #add values to dates
 
@@ -94,14 +95,14 @@ targets_test_2 <- aggregate(observation ~ datetime + site_id + duration + variab
                             reference_datetime = forecast_date,
                             model_id = model_id,
                             site_id = site,
-                            parameter = forecasted_secchi$parameter,
+                            parameter = forecasted_secchi_rolling_avg$parameter,
                             family = 'ensemble',
-                            prediction = forecasted_secchi$prediction,
+                            prediction = forecasted_secchi_rolling_avg$prediction,
                             variable = variable,
                             depth_m = NA,
                             duration = targets$duration[1],
                             project_id = project_id)
-  forecasted_secchi <- data.frame(forecasted_secchi)
+  forecasted_secchi <- data.frame(forecasted_secchi_rolling_avg)
   #-------------------------------------
 
   return(forecast_df)
